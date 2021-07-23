33601862bbdbd2309d5662ed0b01f8819496b74a
EstephaniaCalvoC <2177@holbertonschool.com>
2021-07-22 22:23:55 -0500

    Fix logic

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Define canUnlockAll funtion"""
-from collections import deque
+from collections import namedtuple
 
 
 def canUnlockAll(boxes):
@@ -13,25 +13,29 @@ def canUnlockAll(boxes):
     if n_boxes == 1:
         return True
 
-    lock_state = [1] + [0] * (n_boxes - 1)
-    deque_boxes = [deque(b) for b in boxes]
+    Box = namedtuple('Box', ['keys', 'unlock', 'evaluated'])
+    tuple_boxes = [Box(b, False, False) for b in boxes]
+    tuple_boxes[0] = tuple_boxes[0]._replace(unlock=True)
 
-    current_box = deque_boxes[0]
+    for n in range(len(boxes)):
+        current_box = []
+        idx = 0
 
-    max_jumps, jumps = (n_boxes, 0)
+        for box in tuple_boxes:
+            if box.unlock and not box.evaluated:
+                current_box = box
+                break
+            idx += 1
 
-    while (current_box and jumps <= max_jumps):
-        new_max_jumps = len(current_box) ** n_boxes
-        max_jumps = new_max_jumps if max_jumps < new_max_jumps else max_jumps
+        if not current_box or len(current_box) > n_boxes:
+            return False
 
-        idx_next_box = current_box[0]
-        current_box.rotate(-1)
-        lock_state[idx_next_box] = 1
+        for key in current_box.keys:
+            tuple_boxes[key] = tuple_boxes[key]._replace(unlock = True)
 
-        if all(box for box in lock_state):
+        if all(box.unlock for box in tuple_boxes):
             return True
 
-        current_box = deque_boxes[idx_next_box]
-        jumps += 1
+        tuple_boxes[idx] = current_box._replace(evaluated = True)
 
     return False
